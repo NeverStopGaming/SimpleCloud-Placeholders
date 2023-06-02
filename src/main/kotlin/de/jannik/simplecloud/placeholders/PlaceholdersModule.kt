cc503180eb41c7ef70744977d7b4aa0cfa9dccb5
Jannik <jannik.developer02@gmail.com>
2023-06-02 18:51:29 +0200

    add placholder module structure (not needed because the module is just a spigot plugin basically)

---
+++ src/main/kotlin/de/jannik/simplecloud/placeholders/PlaceholdersModule.kt
@@ -0,0 +1,16 @@
+package de.jannik.simplecloud.placeholders
+
+import eu.thesimplecloud.api.external.ICloudModule
+
+class PlaceholdersModule : ICloudModule {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+
+    override fun isReloadable(): Boolean = false
+}
