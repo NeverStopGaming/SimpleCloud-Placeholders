545ea7d351cc3a8a145b2900245902876073dc80
Jannik <jannik.developer02@gmail.com>
2023-06-02 18:50:49 +0200

    add dependencies and repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,32 @@ allprojects {
     }
 }
 
+repositories {
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://repo.thesimplecloud.eu/artifactory/list/gradle-release-local/")
+
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
+        content {
+            includeGroup("org.bukkit")
+            includeGroup("org.spigotmc")
+        }
+    }
+
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
+}
+
+dependencies {
+    compileOnly("eu.thesimplecloud.simplecloud:simplecloud-api:2.5.0")
+    compileOnly("eu.thesimplecloud.simplecloud:simplecloud-base:2.5.0")
+    compileOnly("eu.thesimplecloud.simplecloud:simplecloud-launcher:2.4.1")
+    compileOnly("eu.thesimplecloud.simplecloud:simplecloud-plugin:2.4.1")
+    compileOnly("me.clip:placeholderapi:2.11.1")
+
+    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("com.github.azbh111:craftbukkit-1.8.8:R")
+}
+
 tasks.test {
     useJUnitPlatform()
 }
---
+++ spigot-test/build.gradle.kts
@@ -0,0 +1,26 @@
+repositories {
+    maven("https://jitpack.io")
+
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
+        content {
+            includeGroup("org.bukkit")
+            includeGroup("org.spigotmc")
+        }
+    }
+
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
+
+    maven("https://repo.thesimplecloud.eu/artifactory/list/gradle-release-local")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+}
+
+dependencies {
+    implementation("com.github.ThatKawaiiSam:Assemble:master-SNAPSHOT")
+
+    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("com.github.azbh111:craftbukkit-1.8.8:R")
+
+    compileOnly("me.clip:placeholderapi:2.11.1")
+
+}
