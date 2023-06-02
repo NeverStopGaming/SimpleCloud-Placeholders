7450c82d533ba160390c584ea65a4b622a049c7d
Jannik <jannik.developer02@gmail.com>
2023-06-02 18:51:11 +0200

    add placeholder hook

---
+++ src/main/kotlin/de/jannik/simplecloud/placeholders/plugin/hook/PlaceholderHook.kt
@@ -0,0 +1,39 @@
+package de.jannik.simplecloud.placeholders.plugin.hook
+
+import eu.thesimplecloud.api.CloudAPI
+import eu.thesimplecloud.plugin.startup.CloudPlugin
+import me.clip.placeholderapi.expansion.PlaceholderExpansion
+import org.bukkit.Bukkit
+import org.bukkit.OfflinePlayer
+import org.bukkit.entity.Player
+
+class PlaceholderHook : PlaceholderExpansion() {
+
+    override fun getIdentifier(): String {
+        return "CLOUD"
+    }
+
+    override fun getAuthor(): String {
+        return "Panda, Jannik"
+    }
+
+    override fun getVersion(): String {
+        return "2.5.0"
+    }
+
+    override fun canRegister(): Boolean {
+        return true
+    }
+
+    override fun persist(): Boolean {
+        return true
+    }
+
+    override fun onRequest(player: OfflinePlayer, params: String): String {
+
+        return if(params == "ONLINE_PLAYERS") CloudAPI.instance.getCloudPlayerManager().getNetworkOnlinePlayerCount().get().toString()
+        else if(params == "MAX_PLAYERS") CloudAPI.instance.getCloudPlayerManager().getCloudPlayer(player.uniqueId).get().getConnectedProxy()?.getOnlineCount().toString()
+        else params
+    }
+
+}
