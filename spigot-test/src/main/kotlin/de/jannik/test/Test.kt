63d00ed0fa1cea8aafd772428874afddba54a119
Panda260 <Panda260@NeverStopGaming.net>
2023-06-26 17:38:59 +0200

    removed assemble

--- spigot-test/src/main/kotlin/de/jannik/test/Test.kt
+++ spigot-test/src/main/kotlin/de/jannik/test/Test.kt
@@ -1,20 +1,13 @@
 package de.jannik.test
 
-import de.jannik.test.assemble.TestAssembleAdapter
 import de.jannik.test.commands.testCommand
-import io.github.thatkawaiisam.assemble.Assemble
-import io.github.thatkawaiisam.assemble.AssembleStyle
 import org.bukkit.plugin.java.JavaPlugin
 
 class Test : JavaPlugin() {
 
     override fun onEnable() {
 
-        val assemble = Assemble(this, TestAssembleAdapter())
-        assemble.ticks = 20
-        assemble.assembleStyle = AssembleStyle.MODERN
-
-        getCommand("test").setExecutor(testCommand())
+                getCommand("test").setExecutor(testCommand())
 
 
     }
